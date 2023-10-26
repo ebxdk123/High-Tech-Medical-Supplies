c2c9315cb1df84f56fc946601dbba159435b22eb
ebxdk123 <129865169+ebxdk123@users.noreply.github.com>
2023-10-26 18:27:32 -0400

    Add files via upload

---
+++ Bathroom_Safety/script.js
@@ -0,0 +1,39 @@
+/*var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 35,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    
+  });*/
+
+
+/* grid-list */
+
+const itemList = document.querySelector('.item-list');
+const gridViewBtn = document.getElementById('grid-active-btn');
+const detailsViewBtn = document.getElementById('details-active-btn');
+
+gridViewBtn.classList.add('active-btn');
+
+gridViewBtn.addEventListener('click', () => {
+    gridViewBtn.classList.add('active-btn');
+    detailsViewBtn.classList.remove('active-btn');
+    itemList.classList.remove('details-active');
+});
+
+detailsViewBtn.addEventListener('click', () => {
+    detailsViewBtn.classList.add('active-btn');
+    gridViewBtn.classList.remove("active-btn");
+    itemList.classList.add("details-active");
+});
+  
